a0e0c6ddbb363c615b90931583ebeaa1360e4f4d
fabri-dev <fabri.vogol@gmail.com>
2024-04-29 13:04:42 -0300

    comienzo ejercicio persona

---
+++ EjercicioPersona/src/Main.java
@@ -0,0 +1,7 @@
+import modelo.Persona;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ EjercicioPersona/src/modelo/Persona.java
@@ -0,0 +1,16 @@
+package modelo;
+
+public class Persona {
+    //atributos
+    String nombre,dni;
+    int edad;
+    char sexo;
+    float peso,altura;
+
+    //constructores
+
+    //get y set
+
+    //metodos
+
+}
