6ce06a46cacdfec3af008ef1b4ed5d8c99804fa4
fabri-dev <fabri.vogol@gmail.com>
2024-05-01 12:04:57 -0300

    continuacion guia

--- guiaColecciones/src/modelo/CArrayList.java
+++ guiaColecciones/src/modelo/CArrayList.java
@@ -2,5 +2,84 @@ package modelo;
 
 import interfaces.IMetodosEspeciales;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class CArrayList implements IMetodosEspeciales {
+    //atributos
+    private ArrayList<Integer> arreglito;
+
+    //constructores
+
+    public CArrayList()
+    {
+        arreglito=new ArrayList<>();
+    }
+
+    //get y set
+
+    public ArrayList<Integer> getArreglito() {
+        return arreglito;
+    }
+
+    //metodos
+
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        boolean flag=false;
+        if (o != null)
+        {
+            if (o instanceof CArrayList)
+            {
+                CArrayList aux= (CArrayList) o;
+                if (this.arreglito.equals(aux.getArreglito()))
+                {
+                    flag=true;
+                }
+            }
+        }
+        return flag;
+    }
+
+    @Override
+    public void agregar(Object o) {
+        if (o != null && o instanceof Integer)
+        {
+            
+        }
+
+    }
+
+    @Override
+    public boolean eliminar(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean buscar(Object o) {
+        return false;
+    }
+
+    @Override
+    public String listar() {
+        return "";
+    }
+
+    @Override
+    public int contar() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "CArrayList{" +
+                "arreglito=" + arreglito +
+                '}';
+    }
 }
